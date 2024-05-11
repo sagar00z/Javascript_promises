e02cd74fcc3bdb3beeb37a33f7572e4123506e7b
Dev.Sagar_ <156512552+sagar00z@users.noreply.github.com>
2024-05-11 13:22:45 +0530

    Update promises.js

--- promises.js
+++ promises.js
@@ -20,4 +20,31 @@ myPromise.catch(function(failed){
     console.log(failed)
 }) // reject promises
 
+// example of coffee shop 
+
+function placeOrder(drink){
+    return new Promise(function(resolve,reject){
+        if(drink==='coffee'){
+            resolve('order of coffee recived')
+        }
+        else{
+            reject('other order reject')
+        }
+    })
+}
+
+function processOrder(order){
+    return new Promise(function(resolve){
+        console.log('order is processing')
+        resolve(`${order} is served`)
+    })
+}
+
+placeOrder('coffee').then(function(orderPlaced){
+    console.log(orderPlaced)
+    let orderIsProcess = processOrder(orderPlaced)
+    return orderIsProcess
+}).then(function(processedOrder){
+    console.log(processedOrder)
+})
 
